d7faf7a6c0c632b584b51a3979c8be4a25c15911
BountyDev <35352233+BountyDev@users.noreply.github.com>
2020-07-12 07:44:57 -0400

    new

--- server.py
+++ server.py
@@ -233,7 +233,7 @@ def server(port):
                     packet.write(2, 'ends')
                     packet.send(games[i].oth(player_id), packet)
                     num = i
-            games.pop(i)
+            games.pop(num)
 
             self.close()
 
