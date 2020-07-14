80998a09f9d00a19c62f331965382ccbcf92b8c4
BountyDev <35352233+BountyDev@users.noreply.github.com>
2020-07-14 16:57:15 -0400

    g

--- server.py
+++ server.py
@@ -51,142 +51,141 @@ def server(port):
       global mes
       mes = message
       packet = Packet()
-      if len(mes) != 0:
-          arr[0] = readstring(mes)
+      arr[0] = readstring(mes)
 
-          if arr[0] == "queue":
-              pid = readint(mes)
-              weapon = readstring(mes)
-              queue.append(pid)
-              wpon.append(weapon)
-              print("Entered Queue")
+      if arr[0] == "queue":
+          pid = readint(mes)
+          weapon = readstring(mes)
+          queue.append(pid)
+          wpon.append(weapon)
+          print("Entered Queue")
 
-              if len(queue) == 2:
-                  print("Match found")
-                  new = {}
-                  random.seed(time.time())
-                  map = random.randint(0,2)
-                  rule = random.randint(0,2)
-                  num = 0
-                  for i in queue:
-                    new[num] = i
-                    packet.clear()
-                    packet.write(2, 'queue')
-                    packet.write(3, len(games))
-                    packet.write(3, num)
-                    packet.write(3, map)
-                    packet.write(3, rule)
-                    for l in wpon:
-                        packet.write(2, l)
-                    packet.send(ids[i], packet)
-                    num+=1
-                  queue.clear()
-                  games["game" + str(len(games))] = Match(new[0], new[1], ids[new[0]], ids[new[1]],weapon, weapon)
+          if len(queue) == 2:
+              print("Match found")
+              new = {}
+              random.seed(time.time())
+              map = random.randint(0,2)
+              rule = random.randint(0,2)
+              num = 0
+              for i in queue:
+                new[num] = i
+                packet.clear()
+                packet.write(2, 'queue')
+                packet.write(3, len(games))
+                packet.write(3, num)
+                packet.write(3, map)
+                packet.write(3, rule)
+                for l in wpon:
+                    packet.write(2, l)
+                packet.send(ids[i], packet)
+                num+=1
+              queue.clear()
+              games["game" + str(len(games))] = Match(new[0], new[1], ids[new[0]], ids[new[1]],weapon, weapon)
 
 
-          if arr[0] == "ping":
-              pid = readint(mes)
-              tm = readint(mes)
-              if pid in ids:
+      if arr[0] == "ping":
+          pid = readint(mes)
+          tm = readint(mes)
+          if pid in ids:
+              packet.clear()
+              packet.write(2,"ping")
+              packet.write(3, tm)
+              packet.write(3, len(ids))
+              packet.send(ids[pid], packet)
+
+      if arr[0] == "leave":
+          pid = readint(mes)
+          if pid in queue:
+              queue.remove(pid)
+
+      if arr[0] == "move":
+          xx = readint(mes)
+          yy = readint(mes)
+          pid = readint(mes)
+          match = readint(mes)
+          pn = readint(mes)
+          #xs = readint(mes)
+          if "game"+str(match) in games:
+              cur = games["game" + str(match)]
+              cur.update(xx,yy,pn)
+
+              send = cur.grab(pn)
+
+              packet.clear()
+              packet.write(2, 'move')
+              packet.write(3, xx)
+              packet.write(3, yy)
+              packet.send(send, packet)
+
+      if arr[0] == "pickup":
+          match = readint(mes)
+          xx = readint(mes)
+          yy = readint(mes)
+          pid = readint(mes)
+
+          if "game"+str(match) in games:
+                cur = games["game" + str(match)]
+                send = cur.grab(pid)
+
+                packet.clear()
+                packet.write(2, 'pickup')
+                packet.write(3, xx)
+                packet.write(3, yy)
+                packet.send(send, packet)
+
+      if arr[0] == "hit":
+          game = readint(mes)
+          pn = readint(mes)
+          hit = readint(mes)
+          if "game" + str(game) in games:
+              cur = games["game" + str(game)]
+              chck = cur.hit(pn, hit)
+
+              players = cur.list()
+
+             # if chck:
+                #  for i in players:
+                #      packet.clear()
+                #      packet.write(2, 'end')
+            #          packet.send(i, packet)
+            #      games.pop("game"+str(game))
+
+      if arr[0] == "end":
+          game = readint(mes)
+
+          if "game" + str(game) in games:
+              cur = games["game" + str(game)]
+
+              players = cur.list()
+
+              for i in players:
                   packet.clear()
-                  packet.write(2,"ping")
-                  packet.write(3, tm)
-                  packet.write(3, len(ids))
-                  packet.send(ids[pid], packet)
+                  packet.write(2, 'end')
+                  packet.send(i, packet)
+              games.pop("game"+str(game))
 
-          if arr[0] == "leave":
-              pid = readint(mes)
-              if pid in queue:
-                  queue.remove(pid)
+      if arr[0] == "shoot":
+          xx = readint(mes)
+          yy = readint(mes)
+          dir = readint(mes)
+          match = readint(mes)
+          pn = readint(mes)
+          type = readstring(mes)
+          if "game"+str(match) in games:
+              cur = games["game" + str(match)]
 
-          if arr[0] == "move":
-              xx = readint(mes)
-              yy = readint(mes)
-              pid = readint(mes)
-              match = readint(mes)
-              pn = readint(mes)
-              #xs = readint(mes)
-              if "game"+str(match) in games:
-                  cur = games["game" + str(match)]
-                  cur.update(xx,yy,pn)
+              send = cur.grab(pn)
 
-                  send = cur.grab(pn)
+              packet.clear()
+              packet.write(2, 'shoot')
+              packet.write(3, xx)
+              packet.write(3, yy)
+              packet.write(3, dir)
+              packet.write(2, type)
+              packet.send(send, packet)
 
-                  packet.clear()
-                  packet.write(2, 'move')
-                  packet.write(3, xx)
-                  packet.write(3, yy)
-                  packet.send(send, packet)
-
-          if arr[0] == "pickup":
-              match = readint(mes)
-              xx = readint(mes)
-              yy = readint(mes)
-              pid = readint(mes)
-
-              if "game"+str(match) in games:
-                    cur = games["game" + str(match)]
-                    send = cur.grab(pid)
-
-                    packet.clear()
-                    packet.write(2, 'pickup')
-                    packet.write(3, xx)
-                    packet.write(3, yy)
-                    packet.send(send, packet)
-
-          if arr[0] == "hit":
-              game = readint(mes)
-              pn = readint(mes)
-              hit = readint(mes)
-              if "game" + str(game) in games:
-                  cur = games["game" + str(game)]
-                  chck = cur.hit(pn, hit)
-
-                  players = cur.list()
-
-                 # if chck:
-                    #  for i in players:
-                    #      packet.clear()
-                    #      packet.write(2, 'end')
-                #          packet.send(i, packet)
-                #      games.pop("game"+str(game))
-
-          if arr[0] == "end":
-              game = readint(mes)
-
-              if "game" + str(game) in games:
-                  cur = games["game" + str(game)]
-
-                  players = cur.list()
-
-                  for i in players:
-                      packet.clear()
-                      packet.write(2, 'end')
-                      packet.send(i, packet)
-                  games.pop("game"+str(game))
-
-          if arr[0] == "shoot":
-              xx = readint(mes)
-              yy = readint(mes)
-              dir = readint(mes)
-              match = readint(mes)
-              pn = readint(mes)
-              type = readstring(mes)
-              if "game"+str(match) in games:
-                  cur = games["game" + str(match)]
-
-                  send = cur.grab(pn)
-
-                  packet.clear()
-                  packet.write(2, 'shoot')
-                  packet.write(3, xx)
-                  packet.write(3, yy)
-                  packet.write(3, dir)
-                  packet.write(2, type)
-                  packet.send(send, packet)
-
-          if packet.Buffer > 0:
-              rec(mes)
+      if packet.Buffer > 0:
+          rec(mes)
 
     class MainServer(asyncore.dispatcher):
       def __init__(self, port):
